7b8fa4c57f12dd3a1f1287145a116d8e04c2cc61
Jay <johnsons.shah@gmail.com>
2021-04-13 10:40:25 +0530

    First successful testing of deploying a droplet using terraform

---
+++ digitalocean_terraform_web1.tf
@@ -0,0 +1,24 @@
+resource "digitalocean_droplet" "jay-digitalocean-terraform-web-server-1" {
+  image = "ubuntu-20-04-x64"
+  name = "jay-digitalocean-terraform-web-server-1"
+  region = "blr1"
+  size = "s-1vcpu-1gb"
+  private_networking = true
+  ssh_keys = [
+    "${var.ssh_fingerprint}"
+  ]
+  connection {
+    type = "ssh"
+    host = self.ipv4_address
+    user = "root"
+    private_key = "${file(var.pvt_key)}"
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      "sudo apt-get update",
+      "sudo apt-get -y install nginx"
+    ]
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,15 @@
+variable "jay_digitalocean_terraform_token" {}
+variable "pub_key" {}
+variable "pvt_key" {}
+variable "ssh_fingerprint" {}
+provider "digitalocean" {
+  token = "${var.jay_digitalocean_terraform_token}"
+}
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
